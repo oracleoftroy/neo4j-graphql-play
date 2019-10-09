d195418baa005c91bc0eba3f3288b8152533c0f5
Marc Gallagher <oracleoftroy@gmail.com>
2019-10-08 19:28:16 -0700

    Creating example graphql endpoint

---
+++ README.md
@@ -0,0 +1,3 @@
+# neo4j-graphql-play
+
+Playground for exploring GraphQL using neo4j-graphql-js.
---
+++ src/graphql.ts
@@ -0,0 +1,39 @@
+import { v1 as neo4j } from 'neo4j-driver';
+import { makeAugmentedSchema } from 'neo4j-graphql-js';
+import { Application } from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+const typeDefs = `
+type Post {
+	id: ID!
+	title: String!
+	body: String!
+	created: DateTime!
+	tags: [Tag] @relation(name: "HAS_TAG", direction: "IN")
+	status: PostStatus
+}
+type Tag {
+	id: ID!
+	name: String!
+	posts: [Post] @relation(name: "HAS_TAG", direction: "OUT")
+}
+type Status {
+	name: String!
+	posts: [PostStatus]
+}
+type PostStatus @relation(name: "HAS_STATUS") {
+	from: Status
+	to: Post
+	created: DateTime!
+}
+`;
+
+export function setupGraphQL(app: Application) {
+	const driver = neo4j.driver(
+		'bolt://localhost:7687',
+		neo4j.auth.basic('imakethismeself-blog', 'imakethismeself-blog'),
+	);
+	const schema = makeAugmentedSchema({ typeDefs });
+	const server = new ApolloServer({ schema, context: { driver } });
+	server.applyMiddleware({ app, path: '/' });
+}
